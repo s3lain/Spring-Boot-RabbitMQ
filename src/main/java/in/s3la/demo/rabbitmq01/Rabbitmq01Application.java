58ce90e254c0427d8fc26695cbabcd0f189933e3
Lain <s3lain.navi@gmail.com>
2022-06-01 22:27:37 -0300

    First Commit

---
+++ src/main/java/in/s3la/demo/rabbitmq01/Rabbitmq01Application.java
@@ -0,0 +1,13 @@
+package in.s3la.demo.rabbitmq01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmq01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rabbitmq01Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/s3la/demo/rabbitmq01/Rabbitmq01ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.s3la.demo.rabbitmq01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rabbitmq01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
